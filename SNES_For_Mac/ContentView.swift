cee0ddb8870f83a063703507ad034028fc5b2875
Ran Kaitoku <haide.lan@smartshare.jp>
2025-11-13 20:02:22 +0900

    Initial Commit

---
+++ SNES_For_Mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SNES_For_Mac
+//
+//  Created by Haide Lan on 2025/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SNES_For_Mac/SNES_For_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNES_For_MacApp.swift
+//  SNES_For_Mac
+//
+//  Created by Haide Lan on 2025/11/13.
+//
+
+import SwiftUI
+
+@main
+struct SNES_For_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
